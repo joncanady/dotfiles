da248b75cdc2955cf5ad0d4b00efd729a66a62ec
Jon Canady <jon@joncanady.com>
2011-11-15 11:08:31 -0500

    Restore my original PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH=/Users/jonc/.rvm/gems/ruby-1.8.7-p330/bin:/Users/jonc/.rvm/gems/ruby-1.8.7-p330@global/bin:/Users/jonc/.rvm/rubies/ruby-1.8.7-p330/bin:/Users/jonc/.rvm/bin:/Users/jonc/code/jekyll/bin:/Users/jonc/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/opt/local/bin:/usr/local/mysql/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/Cellar/node/0.4.12/bin
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
