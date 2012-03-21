e4f14b1ea0fbeaf5b5c0a9bd57a153e3a7fe939e
Jon Canady <jon@joncanady.com>
2012-03-21 10:22:30 -0400

    Add some bundle aliases.
    From "Vendor Everything":
    http://ryan.mcgeary.org/2011/02/09/vendor-everything-still-applies/

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,9 +1,12 @@
-alias r='rvm use 1.8.7'
-
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 
 alias spec='spec _1.3.2_'
-alias be='bundle exec'
 alias rake="noglob rake"
+
+alias b="bundle"
+alias bi="b install --path vendor"
+alias bu="b update"
+alias be="b exec"
+alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
