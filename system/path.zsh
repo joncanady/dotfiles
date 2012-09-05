bea273423f15c6964bcaf24a1fe522be2efa1c77
Jon Canady <jon@joncanady.com>
2012-09-05 09:53:04 -0400

    Replace RVM with rbenv officially.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=/Users/jonc/.rvm/gems/ruby-1.9.3-p0/bin:/Users/jonc/.rvm/gems/ruby-1.9.3-p@global/bin:/Users/jonc/.rvm/rubies/ruby-1.9.3-p0/bin:/Users/jonc/.rvm/bin:/Users/jonc/code/jekyll/bin:/Users/jonc/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/opt/local/bin:/usr/local/mysql/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/Cellar/node/0.4.12/bin
+export PATH=/bin:$HOME/.rbenv/shims:/Users/jonc/code/jekyll/bin:/Users/jonc/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/opt/local/bin:/usr/local/mysql/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/Cellar/node/0.4.12/bin
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
