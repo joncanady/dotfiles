0b2ea051ca4cfbaf00965847082b9ca25b699635
Jon Canady <jon@joncanady.com>
2013-07-08 18:50:25 -0400

    Add the current ./bin to the PATH.
    I don't love this, but it's the way the wind is blowing in Rubyland. Don't run
    that on servers or anything.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=/bin:$HOME/.rbenv/shims:/Users/jonc/code/jekyll/bin:/Users/jonc/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/opt/local/bin:/usr/local/mysql/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/Cellar/node/0.4.12/bin:/usr/local/share/npm/bin
+export PATH=./bin:/bin:$HOME/.rbenv/shims:/Users/jonc/code/jekyll/bin:/Users/jonc/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/opt/local/bin:/usr/local/mysql/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/Cellar/node/0.4.12/bin:/usr/local/share/npm/bin
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
