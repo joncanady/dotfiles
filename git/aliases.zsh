f0ba93f5be823fc0a79dcd9f223c463b93320670
Jon Canady <jon@joncanady.com>
2013-07-08 18:48:15 -0400

    Add `gpr` alias to push & create pull request.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,6 +17,7 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gpr="git push origin HEAD -u && git pull-request"
 
 alias egit="GITHUB_HOST=git.innova-partners.com git"
 
