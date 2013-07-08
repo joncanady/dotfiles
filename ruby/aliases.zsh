f62f1b917218a6717a1b10f07f8bd1c96747fc29
Jon Canady <jon@joncanady.com>
2013-07-08 18:49:28 -0400

    Update ruby aliases:
    * remove unnecessary aliases
    * add `z` for zeus

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,7 +1,3 @@
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
 alias spec='spec _1.3.2_'
 alias rake="noglob rake"
 
@@ -9,6 +5,5 @@ alias b="bundle"
 alias bi="b install --path vendor"
 alias bu="b update"
 alias be="b exec"
-alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
 
-alias powdebug="be rdebug -c"
+alias z="zeus"
